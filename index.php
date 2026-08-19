fe974757621a73873e51c5e4f7193e6a0ef683f0
Arthur_Tomazia <arthur_tomazia@estudante.sesisenai.org.br>
2026-08-19 10:59:48 -0300

    ADD - cadastrar prato

--- index.php
+++ index.php
@@ -6,6 +6,29 @@
 </head>
 <body>
     
+    <h1>Cadastrar Pratos</h1>
+
+    <form action="public/cadastrar.php" method="POST">
+        <label for="nome">Nome do Prato:</label>
+        <input type="text" name="nome">
+        <br>
+        <label for="descricao">Descrição do Prato</label>
+        <input type="text" name="descricao">
+        <br>
+        <label for="preco">Preço do Prato</label>
+        <input type="text" name="preco">
+        <br>
+        <label for="categoria">Categoria do Prato</label>
+        <input type="text" name="categoria">
+        <br>
+        <label for="usuario_compra">Usuário que comprou o prato</label>
+        <input type="text" name="usuario_compra">
+        <br>
+        <button type="submit">Fazer Pedido</button>
+    </form>
+
+
+
 </body>
 </html>
 
--- infra/conexao.php
+++ infra/conexao.php
@@ -11,7 +11,7 @@ $descricao = "descricao";
 $preco = "preco";
 $categoria = "categoria";
 
-$conexao = new mysqli($host, $usuario, $senha, $banco, $nome, $descricap, $preco, $categoria);
+$conexao = new mysqli($host, $usuario, $senha, $banco);
 
 if ($conexao->connect_error) {
     die("Erro na conexão com o banco: " . $conexao->connect_error);
