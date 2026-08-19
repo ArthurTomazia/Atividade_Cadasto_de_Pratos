db20414471d7d73147ac54a598d0f482906c9283
Arthur_Tomazia <arthur_tomazia@estudante.sesisenai.org.br>
2026-08-19 09:51:38 -0300

    Troquei {} por ()

--- database/db.sql
+++ database/db.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE restaurante_tolin_m1;
 USE restaurante_tolin_m1;
 
-CREATE TABLE usuario{
+CREATE TABLE usuario(
     id_usuario int AUTO_INCREMENT PRIMARY KEY NOT NULL,
     nome varchar(45) NOT NULL, 
     emial varchar(45) NOT NULL, 
-};
+);
 
-CREATE TABLE prato{
+CREATE TABLE prato(
     id_prato int AUTO_INCREMENT PRIMARY KEY NOT NULL,
     id_usuario FOREIGN KEY,
     nome varchar(45) NOT NULL, 
     descricao varchar(45) NOT NULL, 
     preco varchar(45) NOT NULL, 
     categoria varchar(45) NOT NULL, 
-};
+);
