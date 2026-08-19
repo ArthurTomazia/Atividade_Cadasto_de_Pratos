41679d33066a786f4852c37a1711ddda6ba43178
rafael_colin <rafael_colin@estudante.sesisenai.org.br>
2026-08-19 09:59:37 -0300

    edit - banco

--- database/db.sql
+++ database/db.sql
@@ -4,14 +4,15 @@ USE restaurante_tolin_m1;
 CREATE TABLE usuario(
     id_usuario int AUTO_INCREMENT PRIMARY KEY NOT NULL,
     nome varchar(45) NOT NULL, 
-    emial varchar(45) NOT NULL, 
+    emial varchar(45) NOT NULL
 );
 
 CREATE TABLE prato(
     id_prato int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    id_usuario FOREIGN KEY,
     nome varchar(45) NOT NULL, 
     descricao varchar(45) NOT NULL, 
     preco varchar(45) NOT NULL, 
-    categoria varchar(45) NOT NULL, 
+    categoria varchar(45) NOT NULL,
+    id_usuario int,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
